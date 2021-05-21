60c23335e112660595b6a6a1fa28d9da9fabdfce
Alistair Smith <alistairsmith01@gmail.com>
2021-05-21 14:58:52 +0100

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,18 @@ module.exports = {
 	env: {
 		browser: true,
 		es2021: true,
-		node: true
+		node: true,
 	},
-	extends: [
-		'plugin:react/recommended',
-		'xo'
-	],
-	parser: '@typescript-eslint/parser',
+	extends: ["plugin:react/recommended", "xo", "xo-typescript", "xo-react"],
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
 		ecmaFeatures: {
-			jsx: true
+			jsx: true,
 		},
 		ecmaVersion: 12,
-		sourceType: 'module'
+		sourceType: "module",
 	},
-	plugins: [
-		'react',
-		'@typescript-eslint'
-	],
-	rules: {
-	}
+	plugins: ["react", "@typescript-eslint"],
+	rules: {},
+	ignorePatterns: ["**/*.js"],
 };
