cf484f3c9eed1019be591ec9f55947a752772a7a
Alistair Smith <alistairsmith01@gmail.com>
2021-05-22 22:09:40 +0100

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import React, {useEffect, useState} from 'react';
-import {DialogOverlay, DialogContent} from '@reach/dialog';
-import {animated} from '@react-spring/web';
 import {useAtom} from 'jotai';
 import {atomCommands} from '../atoms/commands';
 import {flowers} from '../flowers';
 
-export const AnimatedDialogOverlay = animated(DialogOverlay);
-export const AnimatedDialogContent = animated(DialogContent);
-
 const getRandomFlower = () => flowers[Math.floor(Math.random() * flowers.length)];
 
-=======
-import React from 'react';
-import {Command} from '../components/pallette';
-
->>>>>>> 8bcda2057ea7951d74f68b6a24c8e6fcd812c75e
 const Home = () => {
 	const [, set] = useAtom(atomCommands);
 	const [flower, setFlower] = useState(getRandomFlower());
