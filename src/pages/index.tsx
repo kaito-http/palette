8bcda2057ea7951d74f68b6a24c8e6fcd812c75e
Leah Lundqvist <leah@pigeon.sh>
2021-05-22 23:02:21 +0200

    package: remove react-sptring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {DialogOverlay, DialogContent} from '@reach/dialog';
-import {animated} from '@react-spring/web';
 import {Command} from '../components/pallette';
-
-export const AnimatedDialogOverlay = animated(DialogOverlay);
-export const AnimatedDialogContent = animated(DialogContent);
 
 const Home = () => {
 	return <Command />;
